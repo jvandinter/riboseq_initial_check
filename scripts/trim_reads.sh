8811878978d3d3a7a259ed2909cb8a63a5b05e39
jvandinter <jipvandinter@hotmail.nl>
2024-04-02 17:18:02 +0200

    Increase trimming speed
    Use fastp instead of trimgalore, as it is much faster.
    Stil use trimgalore container for the fastQC output.

--- scripts/trim_reads.sh
+++ scripts/trim_reads.sh
@@ -19,35 +19,36 @@ r1_filename=$(basename ${r1_file})
 
 # Create output dirs
 cd "${outdir}"
-mkdir -p "trimgalore/${sample_id}/"
+mkdir -p "trimming/${sample_id}/"
 
 # Check whether script needs to run
-if [[ -f "${outdir}/trimgalore/${sample_id}/${r1_filename}" ]]; then
+if [[ -f "${outdir}/trimming/${sample_id}/${r1_filename}" ]]; then
   echo "`date` ${sample_id} file already present"
   exit 0
 fi
 
 echo "`date` running trimgalore for ${sample_id}"
-apptainer exec -B "/hpc:/hpc" --env "LC_ALL=C.UTF-8" ${container_dir}/trimgalore-0.6.6.sif fastp --version
+apptainer exec -B "/hpc:/hpc" --env "LC_ALL=C.UTF-8" ${container_dir}/fastp_0.23.4--hadf994f_2.sif fastp --version
 apptainer exec -B "/hpc:/hpc" --env "LC_ALL=C.UTF-8" ${container_dir}/trimgalore-0.6.6.sif fastqc --version
 
-cd "${outdir}/trimgalore/${sample_id}/"
+cd "${outdir}/trimming/${sample_id}/"
 
-apptainer exec -B "/hpc:/hpc" --env LC_ALL=C.UTF-8 ${container_dir}/trimgalore-0.6.6.sif trim_galore \
-  "${r1_file}" \
-  --cores 2 \
-  --gzip \
-  --length 25 \
-  --trim-n \
-  --fastqc \
-  --fastqc_args "--outdir ${outdir}/trimgalore/${sample_id}/" \
-  --output_dir "${outdir}/trimgalore/${sample_id}/"
+# Trim reads with fastp
+apptainer exec -B "/hpc:/hpc" --env LC_ALL=C.UTF-8 ${container_dir}/fastp_0.23.4--hadf994f_2.sif fastp \
+  -i "${r1_file}" \
+  -o "${outdir}/trimming/${sample_id}/${bf1}" \
+  -l 25 \
+  --verbose \
+  --thread ${cpu}
 
-mv "${outdir}/trimgalore/${sample_id}/${r1_filename%.*.*}_trimmed.fq.gz" "${outdir}/trimgalore/${sample_id}/${r1_filename}"
+apptainer exec -B "/hpc:/hpc,${TMPDIR}:${TMPDIR}" --env "LC_ALL=C.UTF-8" ${container_dir}/trimgalore-0.6.6.sif fastqc \
+  --outdir "${outdir}/trimming/${sample_id}/" \
+  --dir ${TMPDIR} \
+  --threads ${cpu}
 
 # Calculate trimcounts per paired fastq
 tot_reads=$(zcat "${r1_file}" | echo $((`wc -l`/4)))
-trimmed_reads=$(zcat "${outdir}/trimgalore/${sample_id}/${r1_filename}" | echo $((`wc -l`/4)))
+trimmed_reads=$(zcat "${outdir}/trimming/${sample_id}/${bf1}" | echo $((`wc -l`/4)))
 trimmed_percentage=`awk -vn=248 "BEGIN{print(${trimmed_reads}/${tot_reads}*100)}"`
 
 # Add read trimming info to run QC file
